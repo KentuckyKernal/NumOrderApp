446c5e6868897092ec088415e9e83762ddf7d488
Rafte <Rafte@DESKTOP-BA7C851>
2017-06-03 23:25:41 +0100

    Importing Project

---
+++ CsiOrdNum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CsiOrdNum.Startup))]
+namespace CsiOrdNum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
